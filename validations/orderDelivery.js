af92e6c822d9b1acd45de7dc6715b4f993d17b8b
shahzod-rahimov <rakhimovsh3235@gmail.com>
2023-05-27 01:09:02 +0500

    bug fix

--- validations/orderDelivery.js
+++ validations/orderDelivery.js
@@ -1,49 +1,54 @@
-const {checkSchema} = require("express-validator");
+const { checkSchema } = require("express-validator");
 
 const orderDeliveryCreateValidation = checkSchema({
-    description:{
-        isString:{},
-        errorMessage:"Description must be string type"
-    },
-    stuff_id:{
-        isMongoId:{},
-        errorMessage:"Invalid stuff_id"
-    },
-    order_id:{
-        isMongoId:{},
-        errorMessage:"Invalid order_id"
-    }
+  description: {
+    optional: {},
+    isString: {},
+    errorMessage: "Description must be string type",
+  },
+  staff_id: {
+    isMongoId: {},
+    errorMessage: "Invalid stuff_id",
+  },
+  order_id: {
+    isMongoId: {},
+    errorMessage: "Invalid order_id",
+  },
 });
 
 const orderDeliveryUpdateValidation = checkSchema({
-    description:{
-        isString:{},
-        optional:{},
-        errorMessage:"Description must be string type"
-    },
-    stuff_id:{
-        isMongoId:{},
-        optional:{},
-        errorMessage:"Invalid stuff_id"
-    },
-    order_id:{
-        isMongoId:{},
-        optional:{},
-        errorMessage:"Invalid order_id"
-    }
-})
+  description: {
+    isString: {},
+    optional: {},
+    errorMessage: "Description must be string type",
+  },
+  staff_id: {
+    isMongoId: {},
+    optional: {},
+    errorMessage: "Invalid stuff_id",
+  },
+  order_id: {
+    isMongoId: {},
+    optional: {},
+    errorMessage: "Invalid order_id",
+  },
+});
 
 const queryValidation = checkSchema({
-    stuff:{
-        isMongoId:{},
-        optional:{},
-        errorMessage:"Invalid Mongo ID"
-    },
-    order:{
-        isMongoId:{},
-        optional:{},
-        errorMessage:"Invalid Mongo id Order"
-    }
-})
+  staff: {
+    isMongoId: {},
+    optional: {},
+    errorMessage: "Invalid Mongo ID",
+  },
+  order: {
+    isMongoId: {},
+    optional: {},
+    errorMessage: "Invalid Mongo id Order",
+  },
+});
 
-module.exports = {orderDeliveryCreateValidation,orderDeliveryUpdateValidation,queryValidation}
+module.exports = {
+  orderDeliveryCreateValidation,
+  orderDeliveryUpdateValidation,
+  queryValidation,
+};
